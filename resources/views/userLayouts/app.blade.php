4e3850a1c1bfd6a718b96fcc378e352fdd6e2a84
SamerAljbour <samerseran34@gmail.com>
2026-04-20 11:32:57 +0300

    feat: add user profile management features
    - Implemented profile editing functionality with forms for updating user information, changing passwords, and deleting accounts.
    - Created partial views for updating profile information, updating passwords, and deleting user accounts.
    - Added routes for authentication, including registration, login, password reset, and email verification.
    - Developed tests for authentication, email verification, password confirmation, password reset, and profile management.
    - Introduced Tailwind CSS configuration for styling.
    - Created layout files for user interface, including headers, footers, and navigation.

---
+++ resources/views/userLayouts/app.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>Standout</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- mobile specific metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="{{ asset('user/css/base.css') }}">
+    <link rel="stylesheet" href="{{ asset('user/css/vendor.css') }}">
+    <link rel="stylesheet" href="{{ asset('user/css/main.css') }}">
+
+    <!-- script
+    ================================================== -->
+    <script src="{{ asset('user/js/modernizr.js') }}"></script>
+    <script src="{{ asset('user/js/pace.min.js') }}"></script>
+
+    <!-- favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="{{ asset('user/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('user/favicon.ico') }}" type="image/x-icon">
+
+</head>
+
+<body id="top">
+
+    <div id="preloader">
+        <div id="loader"></div>
+    </div>
+
+
+   @include('layouts.header')
+
+   <main>
+        @yield('content')
+   </main>
+
+    @include('layouts.footer')
+
+    <!-- Java Script
+    ================================================== -->
+    <script src="{{ asset('user/js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('user/js/plugins.js') }}"></script>
+    <script src="{{ asset('user/js/main.js') }}"></script>
+
+
+</body>
+
